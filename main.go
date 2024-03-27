e654478013c3d6bc8b0c2aff0149a63aeaf24c58
Bruno Fioreze <bruno-fioreze-@hotmail.com>
2024-03-27 18:58:23 -0300

    feat: added godotenv

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("Hello, world!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
