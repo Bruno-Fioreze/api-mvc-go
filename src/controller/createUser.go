f23bb9b3d7f63fd6d85e0230758de527e48a67ef
Bruno Fioreze <bruno-fioreze-@hotmail.com>
2024-03-29 10:58:49 -0300

    feat: created model user response and request

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,5 +1,11 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	resterr "github.com/Bruno-Fioreze/api-mvc-go/src/configuration/rest_err"
+	"github.com/gin-gonic/gin"
+)
 
-func CreateUser(c *gin.Context) {}
+func CreateUser(c *gin.Context) {
+	err := resterr.NewBadRequestError("you called the route incorrectly")
+	c.JSON(err.Code, err)
+}
