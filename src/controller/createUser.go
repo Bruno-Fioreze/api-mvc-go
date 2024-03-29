f7f1495b8dfd2a58764a24af3cfafff3257447cf
Bruno Fioreze <bruno-fioreze-@hotmail.com>
2024-03-29 11:16:06 -0300

    feat: implemented route createUser

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,22 @@
 package controller
 
 import (
+	"fmt"
+
 	resterr "github.com/Bruno-Fioreze/api-mvc-go/src/configuration/rest_err"
+	"github.com/Bruno-Fioreze/api-mvc-go/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	err := resterr.NewBadRequestError("you called the route incorrectly")
-	c.JSON(err.Code, err)
+
+	var userRequest request.UserRequest
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		message := fmt.Sprintf("There are some incorrect parameters, error=%s", err.Error())
+		restErr := resterr.NewBadRequestError(message)
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
